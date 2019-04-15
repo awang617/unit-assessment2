699a804242de3f1b54afb824a64c186e044b3ccf
awang617 <wanga18@mail.wlu.edu>
2019-04-15 13:38:40 -0700

    adding a new todo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var todos = require('../data/todos');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'hello', todos },);
 });
+
+/* POST todos */
+router.post('/todos', function(req, res, next) {
+  todos.push({todos: req.body.todo})
+  console.log(todos)
+  res.redirect('/')
+})
 
 module.exports = router;
